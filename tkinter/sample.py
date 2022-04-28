2f3b01998310ec2af6c2c3dd60e2ce505a962832
PhilippKNMergener <60490212+PhilippKNMergener@users.noreply.github.com>
2022-04-28 16:55:10 -0700

    tkinter sample

--- tkinter/sample.py
+++ tkinter/sample.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 
 root = tk.Tk()
-message = tk.Label(text="Welcome to PyChat")
-message.pack()
+root.minsize(800, 600)
+root.maxsize(800, 600)
+
+title = tk.Label(parent=root, text="PyChat")
+title.pack()
+
+chat_box = tk.Frame(parent=root, width=400, height=400)
+chat_box.pack()
+
+input_box = tk.Text(parent=root, width=400, height=100)
 
 root.mainloop()

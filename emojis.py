ff32dabe7b8fb252cf72cbff5c6e47842f27d4dd
cucumberpeel <66458502+cucumberpeel@users.noreply.github.com>
2022-05-10 16:49:19 -0700

    emojify & demojify messages

--- emojis.py
+++ emojis.py
@@ -13,6 +13,14 @@ class Emoji():
         '^_^': u'\U0001f60a', ':(': u'\U0001f641', '>:(': u'\U0001f621', ':|': u'\U0001f610', '-_-': u'\U0001f611', 'B)': u'\U0001f60E', \
         ':/': u'\U0001F615', ':o': u'\U0001f62e', ":'(": u'\U0001f622', 'T_T': u'\U0001f62d', '<3': u'\U0001f90d'}
 
+    """list all available shortcuts"""
+    def get_all_shortcuts(self):
+        print("-" * 20)
+        print("Available emoji shortcuts:")
+        for text, uni in self.shortcuts:
+            print(f"{text} --> {uni}")
+        print("-" * 20)
+
     """emoji shortcut to unicode"""
     def emojify_msg(self, msg):
         words = msg.split()
@@ -23,11 +31,8 @@ class Emoji():
 
     """unicode to emoji shortcut"""
     def demojify_msg(self, msg):
-        pass
-
-    if __name__ == "__main__":
-        msg = 'hi :)'
-        print(emojify_msg(msg))
-
-        for s in self.shortcuts.values():
-            print(emoji.emojize(s))
+        words = msg.split()
+        for i in range(len(words)):
+            if words[i] in self.shortcuts.values():
+                words[i] = list(self.shortcuts.keys())[i]
+        return ' '.join(words)
